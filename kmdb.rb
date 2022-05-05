3173f2c5b2e8f1c9d1291fa69579d1e60aaa29c8
Batuhan SONMEZER <batuhan.sonmezer@kellogg.northwestern.edu>
2022-05-05 17:53:17 +0000

    v2

--- kmdb.rb
+++ kmdb.rb
@@ -84,23 +84,7 @@ Rails.logger.info "----- FRESH START! -----"
 # Do not use hard-coded foreign key IDs.
 # TODO!
 
-new_movie = Movie.new
-new_movie["title"] = "Batman Begins"
-new_movie["year_released"] = "2005"
-new_movie["rated"] = "PG-13"
-new_movie.save
 
-new_movie = Movie.new
-new_movie["title"] = "The Dark Knight"
-new_movie["year_released"] = "2008"
-new_movie["rated"] = "PG-13"
-new_movie.save
-
-new_movie = Movie.new
-new_movie["title"] = "The Dark Knight Rises"
-new_movie["year_released"] = "2012"
-new_movie["rated"] = "PG-13"
-new_movie.save
 
 
 
@@ -112,12 +96,80 @@ new_actor = Actor.new
 new_actor["name"] = "Michael Caine"
 new_actor.save
 
+new_actor = Actor.new
+new_actor["name"] = "Liam Neeson"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Katie Holmes"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Gary Oldman"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Heath Ledger"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Aaron Eckhart"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Michael Caine"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Maggie Gyllenhaal"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Tom Hardy"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Joseph Gordon-Levitt"
+new_actor.save
+
+new_actor = Actor.new
+new_actor["name"] = "Anne Hathaway"
+new_actor.save
+
 
 
 new_studio = Studio.new
 new_studio["name"] = "Warner Bros."
 new_studio.save
 
+new_studio = Studio.new
+new_studio["name"] = "Sony Pictures"
+new_studio.save
+
+
+warner = Studio.find_by({"name" => "Warner Bros."})
+#puts warner.inspect
+
+new_movie = Movie.new
+new_movie["title"] = "Batman Begins"
+new_movie["year_released"] = "2005"
+new_movie["rated"] = "PG-13"
+new_movie["studio_id"] = warner["id"]
+new_movie.save
+
+new_movie = Movie.new
+new_movie["title"] = "The Dark Knight"
+new_movie["year_released"] = "2008"
+new_movie["rated"] = "PG-13"
+new_movie["studio_id"] = warner["id"]
+new_movie.save
+
+new_movie = Movie.new
+new_movie["title"] = "The Dark Knight Rises"
+new_movie["year_released"] = "2012"
+new_movie["rated"] = "PG-13"
+new_movie["studio_id"] = warner["id"]
+new_movie.save
 
 
 new_role = Role.new
@@ -127,6 +179,40 @@ new_role.save
 new_role = Role.new
 new_role["character_name"] = "Alfred"
 new_role.save
+
+new_role = Role.new
+new_role["character_name"] = "Ra's Al Ghul"
+new_role.save
+
+new_role = Role.new
+new_role["character_name"] = "Rachel Dawes"
+new_role.save
+
+new_role = Role.new
+new_role["character_name"] = "Commissioner Gordon"
+new_role.save
+
+new_role = Role.new
+new_role["character_name"] = "Joker"
+new_role.save
+
+new_role = Role.new
+new_role["character_name"] = "Harvey Dent"
+new_role.save
+
+new_role = Role.new
+new_role["character_name"] = "Bane"
+new_role.save
+
+new_role = Role.new
+new_role["character_name"] = "John Blake"
+new_role.save
+
+new_role = Role.new
+new_role["character_name"] = "Selina Kyle"
+new_role.save
+
+
 
 
 # Prints a header for the movies output
